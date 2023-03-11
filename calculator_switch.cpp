78b955de92bda38d533aa6c495897dc616f55132
Akshat Agrawal <94231387+akshhhat@users.noreply.github.com>
2023-03-12 02:33:50 +0530

    Add files via upload

---
+++ calculator_switch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int b;
+    cout << "Enter the value of a" << endl;
+    cin>>a;
+    cout << "Enter the value of b" << endl;
+    cin>>b;
+
+    char op;
+    cout << "Enter the operation you want to perform" << endl;
+cin >> op ;
+
+    switch ( op ){
+ case '+':
+    cout << (a + b) << endl;
+    break;
+case '*':
+    cout << (a * b) << endl;
+    break;
+
+case '%':
+    cout << (a % b) << endl;
+    break;
+
+
+case '-':
+    cout << (a - b) << endl;
+    break;      
+
+case '/':
+    cout << (a / b) << endl;
+    break;
+
+    default : cout << "Please enter a valid operation" << endl;
+}
+}
